f797c1208b7938721a55de5f2a946b729ebb0218
RicardaErath <ricarda.erath@iubh-fernstudium.de>
2024-07-30 15:43:59 +0200

    deleted layout folder

--- layout/script.js
+++ layout/script.js
@@ -1,8 +1,4 @@
-// JavaScript zum Ein- und Ausblenden des Navigation-Menüs
 function toggleMenu() {
     const navMenu = document.getElementById('nav-menu');
     navMenu.classList.toggle('show');
 }
-
-// Event-Listener für das Burger-Menü
-document.getElementById('burger-menu').addEventListener('click', toggleMenu);
